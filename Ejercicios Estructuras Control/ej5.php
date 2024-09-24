cd91e1a570bddc700c2ed181d19bddf41a74dd3d
Ovidiu <JTMaston@protonmail.com>
2024-09-24 13:22:06 +0200

    Fin ejs estructuras de control

--- Ejercicios Estructuras Control/ej5.php
+++ Ejercicios Estructuras Control/ej5.php
@@ -19,17 +19,63 @@
     $j1v=$r1+$r2+$r3;
     $j2v=$r4+$r5+$r6;
     $texto;
-    if (($r1==$r2&&$r2==$r3)&&($r4==$r5&&$r5==$r6)&&($r1==$r4)) {#empate de trios
-        $texto="Ha habido empate de trios.";
-    }
-    elseif (($r1==$r2&&$r2==$r3)&&($r4==$r5&&$r5==$r6)&&($r1>$r4)) {#trio mayor J1
-        $texto="Gana jugador 1 por trio mayor.";
-    }
-    elseif (($r1==$r2&&$r2==$r3)&&($r4==$r5&&$r5==$r6)&&($r1<$r4)) {#trio mayor J2
-        $texto="Gana jugador 2 por trio mayor.";
-    }
-    elseif (($r1==$r2||$r2==$r3||$r3==$r1)<=($r4==$r5&&$r5==$r6)) {#pareja mahor J1
-        $texto="Gana jugador 2 por pareja mayor.";
+    if (($r1==$r2&&$r2==$r3)&&($r4==$r5&&$r5==$r6)) {#ambos tienen trio
+        if ($r1>$r4) {#trio mayor J1
+            $texto="Gana jugador 1 por trio mayor.";
+        } elseif ($r1<$r4) {#trio mayor J2
+            $texto="Gana jugador 2 por trio mayor.";
+        } else{#empate de trios
+            $texto="Ha habido empate de trios.";
+        }
+    }#jugador 1 tiene trio pero 2 no
+    elseif (($r1==$r2&&$r2==$r3)&&!($r4==$r5&&$r5==$r6)) {
+        $texto="Gana jugador 1 por trio.";
+    }#jugador 2 tiene trio pero 1 no
+    elseif (!($r1==$r2&&$r2==$r3)&&($r4==$r5&&$r5==$r6)) {
+        $texto="Gana jugador 2 por trio.";
+    }#ambos tienen alguna pareja
+    elseif (($r1==$r2||$r2==$r3||$r3==$r1)&&($r4==$r5||$r5==$r6||$r6==$r4)) {
+        #extraer las parejas en variables
+        if ($r1==$r2) {#j1
+            $p1=$r1;
+        }elseif ($r2==$r3) {
+            $p1=$r2;
+        }elseif ($r3==$r1) {
+            $p1=$r3;
+        }#j2
+        if ($r4==$r5) {#j1
+            $p2=$r4;
+        }elseif ($r5==$r6) {
+            $p2=$r5;
+        }elseif ($r6==$r4) {
+            $p2=$r6;
+        }#comprar parejas
+        if ($p1>$p2) {#j1 par mayor
+            $texto="Gana jugador 1 por pareja mayor.";
+        }elseif ($p1<$p2) {#j2 par mayor
+            $texto="Gana jugador 2 por pareja mayor.";
+        }else {#parejas iguales
+            if ($j1v>$j2v) {
+                $texto="Gana jugador 1 por puntuación.";
+            }elseif ($j1v<$j2v) {
+                $texto="Gana jugador 2 por puntuación.";
+            }else {
+                $texto="Empate por puntuación.";
+            }
+        }
+        
+    }elseif(($r1==$r2||$r2==$r3||$r3==$r1)&&!($r4==$r5||$r5==$r6||$r6==$r4)){#j1 tiene pareja pero 2 no
+        $texto="Gana jugador 1 por pareja.";
+    }elseif(!($r1==$r2||$r2==$r3||$r3==$r1)&&($r4==$r5||$r5==$r6||$r6==$r4)){#j2 tiene pareja pero 1 no
+        $texto="Gana jugador 2 por pareja.";
+    }else {#no hay ni pareja ni trio
+        if ($j1v>$j2v) {    
+            $texto="Gana jugador 1 por puntuación.";
+        }elseif ($j1v<$j2v) {
+            $texto="Gana jugador 2 por puntuación.";
+        }else {
+            $texto="Empate por puntuación.";
+        }
     }
     print $texto;
 ?>
